8f9074b39c1d838cd3005200c3d138329b241609
TrueDev01 <superhollywood0229@gmail.com>
2022-06-04 15:26:21 -0700

    db url update

--- app.js
+++ app.js
@@ -36,8 +36,8 @@ app.use('/api/users', users);
 
 
 
-// const url = `mongodb+srv://arianna:freearianna@cluster0.lbdtqsd.mongodb.net/test`;
-const url = 'mongodb+srv://superhollywood:kpsecret0229@cluster0.kwk4fdy.mongodb.net/userList?retryWrites=true&w=majority'
+const url = `mongodb+srv://arianna:freearianna@cluster0.lbdtqsd.mongodb.net/test`;
+// const url = 'mongodb+srv://superhollywood:kpsecret0229@cluster0.kwk4fdy.mongodb.net/userList?retryWrites=true&w=majority'
 
 
 const connectionParams={
