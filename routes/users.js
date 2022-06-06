3219c2694edab3120a5fb6feb217c08992d8be9e
TrueDev01 <superhollywood0229@gmail.com>
2022-06-06 19:10:21 +0000

    user schema updated

--- routes/users.js
+++ routes/users.js
@@ -10,194 +10,11 @@ router.get('/', (req, res) => {
     .catch(err => res.status(404).json({ error: 'No user found' }));
 });
 
-// Incase we need this API
-router.get('/:userId', (req, res) => {
-  User.findById({ _id: req.params.userId })
-    .then(user => res.status(200).json(user))
-    .catch(err => res.status(404).json({ error: 'No user found by this id' }));
-});
-
 router.get('/profile/:userId', (req, res) => {
   Profile.findById({ _id: req.params.userId })
     .then(user => res.status(200).json(user))
     .catch(err => res.status(404).json({ error: 'No user found by this id' }));
 });
-
-// search by Fname, Lname, Sex, and Age -- { query: input }
-// Just naive search, for high level search, need rewrite
-router.post('/search', (req, res) => {
-  User.find()
-    .then(users =>
-      users.filter(
-        user =>
-          user.firstname
-            .toLowerCase()
-            .indexOf(req.body.query.toString().toLowerCase()) !== -1 ||
-          user.lastname
-            .toLowerCase()
-            .indexOf(req.body.query.toString().toLowerCase()) !== -1 ||
-          user.sex
-            .toLowerCase()
-            .indexOf(req.body.query.toString().toLowerCase()) !== -1 ||
-          user.age.toString().indexOf(req.body.query.toString()) !== -1
-      )
-    )
-    .then(users => res.status(200).json(users))
-    .catch(err => res.status(404).json({ error: 'No user found' }));
-});
-
-// Sort user by fn, ln, sex, age -- by attribute
-router.post('/sort', (req, res) => {
-  // console.log(req.body.attribute);
-  User.find()
-    .then(users => {
-      switch (req.body.attribute) {
-        // Don't change the USERS array!
-        case 'firstname':
-          return [...users].sort((a, b) =>
-            a.firstname > b.firstname
-              ? 1
-              : a.firstname === b.firstname
-              ? a.lastname > b.lastname
-                ? 1
-                : a.lastname === b.lastname
-                ? a.age > b.age
-                  ? 1
-                  : a.age === b.age
-                  ? a.sex > b.sex
-                    ? 1
-                    : -1
-                  : -1
-                : -1
-              : -1
-          );
-
-        case 'lastname':
-          return [...users].sort((a, b) =>
-            a.lastname > b.lastname
-              ? 1
-              : a.lastname === b.lastname
-              ? a.firstname > b.firstname
-                ? 1
-                : a.firstname === b.firstname
-                ? a.age > b.age
-                  ? 1
-                  : a.age === b.age
-                  ? a.sex > b.sex
-                    ? 1
-                    : -1
-                  : -1
-                : -1
-              : -1
-          );
-
-        case 'sex':
-          return [...users].sort((a, b) =>
-            a.sex > b.sex
-              ? 1
-              : a.sex === b.sex
-              ? a.firstname > b.firstname
-                ? 1
-                : a.firstname === b.firstname
-                ? a.lastname > b.lastname
-                  ? 1
-                  : a.lastname === b.lastname
-                  ? a.age > b.age
-                    ? 1
-                    : -1
-                  : -1
-                : -1
-              : -1
-          );
-
-        case 'age':
-          // console.log([...users][0].age);
-          return [...users].sort((a, b) =>
-            a.age > b.age
-              ? 1
-              : a.age === b.age
-              ? a.firstname > b.firstname
-                ? 1
-                : a.firstname === b.firstname
-                ? a.lastname > b.lastname
-                  ? 1
-                  : a.lastname === b.lastname
-                  ? a.sex > b.sex
-                    ? 1
-                    : -1
-                  : -1
-                : -1
-              : -1
-          );
-
-        default:
-          return [...users];
-      }
-    })
-    .then(users => res.status(200).json(users))
-    .catch(err => res.status(500).json({ error: 'Failed to sort' }));
-});
-
-// Create user
-router.post('/', (req, res) => {
-  const newUser = new User({
-    firstname: req.body.firstname,
-    lastname: req.body.lastname,
-    email: req.body.email,
-    phone: req.body.phone,
-    password: req.body.password
-  });
-  newUser
-    .save()
-    .then(user => res.status(200).json(user))
-    .catch(err => res.status(500).json({ error: 'Failed to create' + err }));
-});
-
-// Create recall User
-router.post('/recall', (req, res) => {
-  const newRecallUser = new RecallUser({
-    firstname: req.body.firstname,
-    lastname: req.body.lastname,
-    email: req.body.email,
-    phone: req.body.phone,
-    zipcode: req.body.zipcode,
-    state: req.body.user_state,
-    profile_id: req.body.person,
-    address: req.body.address
-  });
-  newRecallUser
-    .save()
-    .then(user => res.status(200).json(user))
-    .catch(err => res.status(500).json({ error: 'Failed to create' + err }));
-});
-
-
-// Edit user
-// router.put('/:userId', (req, res) => {
-//   User.findByIdAndUpdate(req.params.userId, {
-//     $set: {
-//       firstname: req.body.firstname,
-//       lastname: req.body.lastname,
-//       sex: req.body.sex,
-//       age: req.body.age,
-//       password: req.body.password
-//     }
-//   })
-//     .then(user => res.status(200).json(user))
-//     .catch(err => res.status(500).json({ error: 'Failed to edit' }));
-// });
-
-
-
-// Delete user
-router.delete('/:userId', (req, res) => {
-  User.findByIdAndDelete({ _id: req.params.userId })
-    .then(user => res.status(200).json(user))
-    .catch(err => res.status(500).json({ error: 'Failed to delete' }));
-});
-
-
-
 
 router.post('/createprofile', (req, res) => {
   const newProfile = new Profile({
@@ -267,6 +84,187 @@ router.get('/findprofile/:query', (req, res) => {
       }
   })
 })
+
+// Incase we need this API
+router.get('/:userId', (req, res) => {
+  User.findById({ _id: req.params.userId })
+    .then(user => res.status(200).json(user))
+    .catch(err => res.status(404).json({ error: 'No user found by this id' }));
+});
+
+// search by Fname, Lname, Sex, and Age -- { query: input }
+// Just naive search, for high level search, need rewrite
+router.post('/search', (req, res) => {
+  User.find()
+    .then(users =>
+      users.filter(
+        user =>
+          user.firstname
+            .toLowerCase()
+            .indexOf(req.body.query.toString().toLowerCase()) !== -1 ||
+          user.lastname
+            .toLowerCase()
+            .indexOf(req.body.query.toString().toLowerCase()) !== -1 ||
+          user.email
+            .toLowerCase()
+            .indexOf(req.body.query.toString().toLowerCase()) !== -1 ||
+          user.phone
+            .toLowerCase()
+            .indexOf(req.body.query.toString().toLowerCase()) !== -1
+      )
+    )
+    .then(users => res.status(200).json(users))
+    .catch(err => res.status(404).json({ error: 'No user found' }));
+});
+
+// Sort user by fn, ln, sex, age -- by attribute
+router.post('/sort', (req, res) => {
+  // console.log(req.body.attribute);
+  User.find()
+    .then(users => {
+      switch (req.body.attribute) {
+        // Don't change the USERS array!
+        case 'firstname':
+          return [...users].sort((a, b) =>
+            a.firstname > b.firstname
+              ? 1
+              : a.firstname === b.firstname
+              ? a.lastname > b.lastname
+                ? 1
+                : a.lastname === b.lastname
+                ? a.email > b.email
+                  ? 1
+                  : a.email === b.email
+                  ? a.phone > b.phone
+                    ? 1
+                    : -1
+                  : -1
+                : -1
+              : -1
+          );
+
+        case 'lastname':
+          return [...users].sort((a, b) =>
+            a.lastname > b.lastname
+              ? 1
+              : a.lastname === b.lastname
+              ? a.firstname > b.firstname
+                ? 1
+                : a.firstname === b.firstname
+                ? a.email > b.email
+                  ? 1
+                  : a.email === b.email
+                  ? a.phone > b.phone
+                    ? 1
+                    : -1
+                  : -1
+                : -1
+              : -1
+          );
+
+        case 'email':
+          return [...users].sort((a, b) =>
+            a.email > b.email
+              ? 1
+              : a.email === b.email
+              ? a.firstname > b.firstname
+                ? 1
+                : a.firstname === b.firstname
+                ? a.lastname > b.lastname
+                  ? 1
+                  : a.lastname === b.lastname
+                  ? a.phone > b.phone
+                    ? 1
+                    : -1
+                  : -1
+                : -1
+              : -1
+          );
+
+        case 'phone':
+          // console.log([...users][0].age);
+          return [...users].sort((a, b) =>
+            a.phone > b.phone
+              ? 1
+              : a.phone === b.phone
+              ? a.firstname > b.firstname
+                ? 1
+                : a.firstname === b.firstname
+                ? a.lastname > b.lastname
+                  ? 1
+                  : a.lastname === b.lastname
+                  ? a.email > b.email
+                    ? 1
+                    : -1
+                  : -1
+                : -1
+              : -1
+          );
+
+        default:
+          return [...users];
+      }
+    })
+    .then(users => res.status(200).json(users))
+    .catch(err => res.status(500).json({ error: 'Failed to sort' }));
+});
+
+// Create user
+router.post('/', (req, res) => {
+  console.log(req.body);
+  const newUser = new User({
+    firstname: req.body.firstName,
+    lastname: req.body.lastName,
+    email: req.body.email,
+    phone: req.body.phone,
+    password: req.body.password
+  });
+  newUser
+    .save()
+    .then(user => res.status(200).json(user))
+    .catch(err => res.status(500).json({ error: 'Failed to create' + err }));
+});
+
+// Delete user
+router.delete('/:userId', (req, res) => {
+  User.findByIdAndDelete({ _id: req.params.userId })
+    .then(user => res.status(200).json(user))
+    .catch(err => res.status(500).json({ error: 'Failed to delete' }));
+});
+
+// Create recall User
+router.post('/recall', (req, res) => {
+  const newRecallUser = new RecallUser({
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    email: req.body.email,
+    phone: req.body.phone,
+    zipcode: req.body.zipcode,
+    state: req.body.user_state,
+    profile_id: req.body.person,
+    address: req.body.address
+  });
+  newRecallUser
+    .save()
+    .then(user => res.status(200).json(user))
+    .catch(err => res.status(500).json({ error: 'Failed to create' + err }));
+});
+
+
+// Edit user
+// router.put('/:userId', (req, res) => {
+//   User.findByIdAndUpdate(req.params.userId, {
+//     $set: {
+//       firstname: req.body.firstname,
+//       lastname: req.body.lastname,
+//       sex: req.body.sex,
+//       age: req.body.age,
+//       password: req.body.password
+//     }
+//   })
+//     .then(user => res.status(200).json(user))
+//     .catch(err => res.status(500).json({ error: 'Failed to edit' }));
+// });
 
 router.get('/findsigner/:query', (req, res) => {
   const query = req.params.query;
